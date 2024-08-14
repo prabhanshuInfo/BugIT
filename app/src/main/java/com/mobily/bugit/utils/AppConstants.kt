5ccac5b66bcb5e86785dc4dee57e0349a8caced3
Prabhanshu.Chaudhary <prabhanshu.chaudhary@infogain.com>
2024-08-14 10:12:03 +0530

    1. Remove the Notion Token and google oauth client key from github repository

--- app/src/main/java/com/mobily/bugit/utils/AppConstants.kt
+++ app/src/main/java/com/mobily/bugit/utils/AppConstants.kt
@@ -2,9 +2,9 @@ package com.mobily.bugit.utils
 
 class AppConstants {
     companion object{
-        internal const val NOTION_TOKEN = "Bearer secret_zCQ18OPZ8EnKRBKZTPZXwYS4pVc6wKuEoXTaaMLlI8X"
-        internal const val NOTION_VERSION = "2022-06-28"
-        internal const val NOTION_DATABASE_ID = "d44c898108b846dfbc3844ba5bbee758"
+        internal const val NOTION_TOKEN = ""
+        internal const val NOTION_VERSION = ""
+        internal const val NOTION_DATABASE_ID = ""
 
     }
 }
